2e441901203ae254ae0feb7517280a2ec2f413e7
Nghi2001 <duynghikhum9@gmail.com>
2022-08-11 17:26:58 +0700

    3

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -1,29 +1,58 @@
-import jwt from "jsonwebtoken";
-import e, { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 import { Secret } from "jsonwebtoken";
+import axios from "axios";
 
-export default function verifyToken (req:Request,res:Response,next:NextFunction) {
+export default async function verifyToken (req:Request,res:Response,next:NextFunction) {
     try {
         let status , msg;
         let token = req.cookies.token;
         if(token) {
-            let decode = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET as Secret);
-            console.log(decode);
-            next()
+            await jwt.verify(token,process.env.ACCESS_TOKEN_SECRET as Secret,async (err:any,decode:any) => {
+                if(err) {
+                    if(req.cookies.refresh_token) {
+                            
+                            let refresh_token = req.cookies.refresh_token;
+
+                            let decode = (await jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET as Secret) as any)
+                            console.log('decode',decode);
+                            
+                            let newToken:any = await  axios.post(`${process.env.AUTH_SERVICE}auth/resetToken`,{
+                                            refresh_token: refresh_token,
+                                            user_id: decode._id
+                                        }, )
+                                console.log(newToken.data);
+                                res.clearCookie('token');
+                                res.clearCookie("refresh_token");
+                                res.cookie('token',newToken.data.accessToken.toString(),{maxAge: 1000*60*60*24*30*30,httpOnly: true})
+                                res.cookie('refresh_token',newToken.data.refreshToken.toString(),{maxAge: 1000*60*60*24*30,httpOnly: true});
+                                
+                    } else {
+                        status = 401; msg = 'Forbiden'
+                    }
+                } else {
+                    status = 200;
+                }
+            })
             
         } else {
             status = 401;
             msg = "Forbiden"
         }
-
-        res.json({
-            status,
-            msg
-        })
+        console.log('next');
+        
+        if( status != 200) {
+            return res.json({
+                status,
+                msg
+            })
+        } else {
+            return next()
+        }
         
     } catch (error) {
         console.log(error);
-        res.status(500).json({
+        return res.status(500).json({
             status:500,
             msg: error
         })
--- src/routes/AuthServiceRoute.ts
+++ src/routes/AuthServiceRoute.ts
@@ -6,14 +6,17 @@ import verifyToken from '../middlewares/verifyToken';
 const router = Express.Router();
 const authServiceProxy = httpProxy(`${AuthApi}`)
 // console.log(AuthApi);
+router.use(verifyToken)
 
+router.get("/auth/listToken/:id", authServiceProxy)
 router.post('/auth/sigin',authServiceProxy);
 router.post('/auth/sigup',authServiceProxy);
 router.post('/auth/changepass',authServiceProxy);
 router.post('/auth/active', authServiceProxy);
 router.post('/', verifyToken,(req,res) => {
-    // console.log(req.cookies);
-    res.json('khdbv')
+    console.log(req.cookies);
+    res.send('jjd')
+    return
     
 })
 
